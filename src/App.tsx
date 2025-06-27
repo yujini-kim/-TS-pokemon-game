e2837b982b74db8506e8e9e41e78d8cc2d1af0dc
yujin <kingyujin56@gmail.com>
2025-06-27 17:18:14 +0900

    Chore: react query 설치 및 기본세팅

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+function App() {
+  const queryClient = new QueryClient()
+
+  return <QueryClientProvider client={queryClient}></QueryClientProvider>
+}
+
+export default App
