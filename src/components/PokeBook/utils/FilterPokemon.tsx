cfc7facb95b43180323d036502efe1b364e93a5e
yujin <kingyujin56@gmail.com>
2025-06-30 16:48:01 +0900

    refactor: coderrabbit 리뷰 반영(모달, 필터함수)

--- src/components/PokeBook/utils/FilterPokemon.tsx
+++ src/components/PokeBook/utils/FilterPokemon.tsx
@@ -1,7 +1,10 @@
-import usePokemonList from '../../../hooks/usePokemonList'
-const { data } = usePokemonList()
+import type { InfiniteData } from '@tanstack/react-query'
+import type { fetchPokemonList } from '../../../types/pokemon'
 
-export function typeFilter(e: React.MouseEvent<HTMLButtonElement>) {
+export function typeFilter(
+  e: React.MouseEvent<HTMLButtonElement>,
+  data: InfiniteData<fetchPokemonList> | undefined,
+) {
   const elementId = e.currentTarget.id
   const filterType = data?.pages.map((item) =>
     item.data.filter((pokemon) => pokemon.koreaTypeName?.includes(elementId)),
@@ -10,7 +13,7 @@ export function typeFilter(e: React.MouseEvent<HTMLButtonElement>) {
   return filterType
 }
 
-export function searchName(searchTerm: string) {
+export function searchName(searchTerm: string, data: InfiniteData<fetchPokemonList> | undefined) {
   const filterName = data?.pages.map((page) =>
     page.data.filter((pokemon) => pokemon.koreaName.includes(searchTerm)),
   )
