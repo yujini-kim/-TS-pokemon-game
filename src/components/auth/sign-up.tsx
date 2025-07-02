3b603e70e5557e855d88ab3882c037a4c4a27685
yujin <kingyujin56@gmail.com>
2025-07-02 15:33:05 +0900

    feat: 회원가입 기능 생성
    - zod 사용하여 유효성 검사
    - InputField 컴포넌트 생성

--- src/components/auth/sign-up.tsx
+++ src/components/auth/sign-up.tsx
@@ -1,19 +1,51 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth'
 import { useForm } from 'react-hook-form'
 import { auth } from '../../utils/firebase'
-
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+import InputField from './components/input-field'
 interface SignUpForm {
   username: string
   email: string
   password: string
+  passwordConfirm: string
 }
+
+const schema = z
+  .object({
+    username: z.string().min(1, '이름을 입력해주세요'),
+    email: z.string().min(1, '이메일을 입력해주세요').email('유효한 이메일을 입력해주세요'),
+    password: z.string().min(6, '비밀번호는 6자 이상 입력해주세요'),
+    passwordConfirm: z.string().min(6, '비밀번호 확인은 6자 이상 입력해주세요'),
+  })
+  .superRefine(({ password, passwordConfirm }, ctx) => {
+    if (password !== passwordConfirm) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'password not matched',
+        path: ['checkPassword'],
+      })
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'password not matched',
+        path: ['password'],
+      })
+    }
+  })
 
 export default function SignUp() {
   const {
     register,
     handleSubmit,
     formState: { errors }, //유효성 검사 통과 여부 저장, 에러정보를 담고 있는 상태값
-  } = useForm<SignUpForm>()
+  } = useForm<SignUpForm>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      username: 'your name',
+      email: 'email@email.com',
+      password: '',
+    },
+  })
 
   const onSubmit = async (data: SignUpForm) => {
     try {
@@ -28,33 +60,36 @@ export default function SignUp() {
     <div className='w-full p-4'>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className='w-1/3'>
-          <label htmlFor='username'>Username</label>
-          <input
-            {...register('username', { required: true })}
-            type='text'
-            id='username'
-            className='border'
+          <InputField<SignUpForm>
+            label={'username'}
+            id={'username'}
+            type={'text'}
+            register={register}
+            error={errors.username?.message}
           />
-          <label htmlFor='email'>E-mail</label>
-          <input
-            {...register('email', { required: true })}
-            type='email'
-            id='email'
-            className='border'
+
+          <InputField<SignUpForm>
+            label={'email'}
+            id={'email'}
+            type={'email'}
+            register={register}
+            error={errors.email?.message}
           />
-          <label htmlFor='password'>Password</label>
-          <input
-            {...register('password', { required: true })}
-            type='password'
-            id='password'
-            className='border'
+
+          <InputField<SignUpForm>
+            label={'password'}
+            id={'password'}
+            type={'password'}
+            register={register}
+            error={errors.password?.message}
           />
-          <label htmlFor='password'>Password Comform</label>
-          <input
-            {...register('password', { required: true })}
-            type='password'
-            id='password'
-            className='border'
+
+          <InputField<SignUpForm>
+            label={'passwordConfirm'}
+            id={'passwordConfirm'}
+            type={'password'}
+            register={register}
+            error={errors.passwordConfirm?.message}
           />
           <button className='bg-blue-400 p-2 rounded-lg text-white mt-4'>Submit</button>
         </div>
