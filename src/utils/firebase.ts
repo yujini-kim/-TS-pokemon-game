ba09d0666c91eebce22a3bd8107c35959e628069
yujin <kingyujin56@gmail.com>
2025-07-02 10:22:43 +0900

    fix: 환경변수 설정 오류 수정

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -8,13 +8,13 @@ import { getAuth } from 'firebase/auth'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.apiKey,
+  apiKey: import.meta.env.VITE_apiKey,
   authDomain: 'ts-pokemon-game.firebaseapp.com',
   projectId: 'ts-pokemon-game',
   storageBucket: 'ts-pokemon-game.firebasestorage.app',
-  messagingSenderId: import.meta.env.messagingSenderId,
-  appId: import.meta.env.appId,
-  measurementId: import.meta.env.measurementId,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId: import.meta.env.VITE_measurementId,
 }
 
 // Initialize Firebase
