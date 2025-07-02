d5ea475f5404b894f3cbe2c7a779f8ff9869bd4f
yujin <kingyujin56@gmail.com>
2025-07-02 15:41:47 +0900

    feat.로그인 기능 추가

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import PokeBook from './components/PokeBook/PokeBook'
 import SignUp from './components/auth/sign-up'
+import SignIn from './components/auth/sign-in'
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/sign-in' element={<SignIn />} />
         <Route path='/' element={<PokeBook />} />
       </Routes>
     </BrowserRouter>
